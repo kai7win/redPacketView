79eb063a6493d1926135fcb9c49177a5bf93cc3e
KaiChi <34007164+kai7win@users.noreply.github.com>
2022-12-07 13:40:20 +0800

    Initial Commit

---
+++ redPacketView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  redPacketView
+//
+//  Created by Thomas on 2022/12/7.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
